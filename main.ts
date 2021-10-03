04dff3d515ad415127c8529220bb88ccee26824e
jorgepiter <90303967+jorgepiter@users.noreply.github.com>
2021-10-03 10:01:59 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,123 @@
+function animacion2 () {
+    for (let index = 0; index < 2; index++) {
+        basic.showLeds(`
+            . # . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . # .
+            . # . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . . . .
+            . . . # .
+            . # . . #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            # . . . .
+            . . . # .
+            . # . . #
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . # . .
+            . . . # .
+            . # . . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            . . # # .
+            `)
+    }
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showIcon(IconNames.Cow)
+    basic.clearScreen()
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    animacion2()
+    basic.clearScreen()
+    basic.showString("Hidratado")
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showString("T")
+    basic.clearScreen()
+    basic.showNumber(Temp_esca)
+    basic.showString("H")
+    basic.clearScreen()
+    basic.showNumber(Humedad)
+})
+function animacion () {
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . # . .
+        . # . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        . . # . .
+        . . . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        . . . . .
+        `)
+}
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("name", Temp_esca)
+})
+let Temp = 0
+let Humedad = 0
+let Temp_esca = 0
+radio.setGroup(1)
+basic.forever(function () {
+    Temp = pins.map(
+    pins.analogReadPin(AnalogPin.P0),
+    0,
+    1023,
+    0,
+    100
+    )
+    Temp_esca = 45 * (Temp / 100)
+    Humedad = pins.map(
+    pins.analogReadPin(AnalogPin.P1),
+    0,
+    1023,
+    0,
+    100
+    )
+    animacion()
+    if (Temp_esca >= 38 && Humedad <= 20) {
+        basic.showString("Válvula ON")
+        radio.sendNumber(0)
+        basic.pause(500)
+        pins.analogWritePin(AnalogPin.P1, 710)
+    }
+})
